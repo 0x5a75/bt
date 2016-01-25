4235796fa88f8829994bb28e160768e835cc5e58
0x5a75 <lovechan@aliyun.com>
2016-01-25 15:43:14 +0800

    add

--- wsgi.py
+++ wsgi.py
@@ -6,6 +6,14 @@
   Created: 2014/3/28
 """
 import os
+
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
 import json
 import logging
 import urllib2
